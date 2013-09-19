d0fd979fadd6dc0a15d0c83a45988e5b884519cf
Vinicius Lima <vinicius@r18.com.br>
2013-09-19 13:49:59 -0300

    Using Fuzzy Format to set tab/space indentation. Using Smart Tabs. Using Whitespace (shortcut to C-c w and C-c C-w for options).

--- emacsconf.el
+++ emacsconf.el
@@ -35,17 +35,18 @@
 
 ; Shortcut to resize buffer to 80 columns
 (defun fix-window-horizontal-size (width)
-  "Set the window's size to 80 (or prefix arg WIDTH) columns wide."
-  (interactive "P")
-  (enlarge-window (- (or width 80) (window-width)) 'horizontal))
+    "Set the window's size to 80 (or prefix arg WIDTH) columns wide."
+    (interactive "P")
+    (enlarge-window (- (or width 80) (window-width)) 'horizontal))
 (global-set-key (kbd "C-x W") 'fix-window-horizontal-size)
 
 ; Configuring vertical indicator to 80 columns
 (require 'fill-column-indicator)
 (add-hook 'after-change-major-mode-hook 'fci-mode)
-(add-hook 'after-change-major-mode-hook (lambda()
-					  (setq fci-rule-column 80)
-					  (setq fci-rule-use-dashes nil)
+(add-hook 'after-change-major-mode-hook
+    (lambda()
+        (setq fci-rule-column 80)
+        (setq fci-rule-use-dashes nil)
 ))
 
 ; Setting line highlight
@@ -56,7 +57,30 @@
 (setq linum-format "%3s ")
 
 ; Setting default indentation of python-hook
-(add-hook 'python-mode-hook (lambda () (setq python-indent 4)))
+(add-hook 'python-mode-hook
+    (lambda ()
+        (setq python-indent 4)
+        (setq tab-width (default-value 'tab-width))
+        (setq indent-tabs-mode nil)
+))
+
+; Configuring Whitespace
+(require 'whitespace)
+(global-set-key (kbd "C-c w") 'whitespace-mode)
+(global-set-key (kbd "C-c C-w") 'whitespace-toggle-options)
+(add-hook 'python-mode-hook
+    (lambda()
+        (setq whitespace-line-column '120)
+))
+
+; Configuring Fuzzy Format (auto set tab/space indentation based on file)
+(require 'fuzzy-format)
+(setq fuzzy-format-default-indent-tabs-mode nil)
+(global-fuzzy-format-mode t)
+
+; Configuring Smart Tabs
+(require 'smart-tabs-mode)
+(smart-tabs-advice python-indent-line-1 python-indent)
 
 ; Configuring JS2-mode
 (autoload 'js2-mode "js2" nil t)
@@ -70,14 +94,15 @@
 ; Configuring Web-mode
 (require 'web-mode)
 (defun web-mode-hook ()
-  "Hooks for Web mode."
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-code-indent-offset 4)
-;  (setq web-mode-indent-style 2)
-  (add-hook 'local-write-file-hooks
-            (lambda ()
-	      (delete-trailing-whitespace)
-	      nil))
+    "Hooks for Web mode."
+    (setq web-mode-markup-indent-offset 2)
+    (setq web-mode-code-indent-offset 4)
+;    (setq web-mode-indent-style 2)
+    (add-hook 'local-write-file-hooks
+        (lambda ()
+	    (delete-trailing-whitespace)
+	    nil
+    ))
 )
 (add-hook 'web-mode-hook  'web-mode-hook)
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
